20cc28b93785e7f3866cab45f0449f4786ac3365
tom_jing <tom_jing@compal.com>
2016-10-07 15:56:27 +0800

    add mean example

--- example/array_col_0/example.py
+++ example/array_col_0/example.py
@@ -1,9 +1,12 @@
 #coding=utf-8
 
 from numpy import genfromtxt
+import numpy as np  # for mean
+
 Array2D = genfromtxt('my_file.csv', delimiter=',')
 
 # get an entire column
 Col0= Array2D[:,0]
 print "Array2D=", Array2D 
 print "Col0=",Col0 
+print "mean=",np.mean(Col0)
